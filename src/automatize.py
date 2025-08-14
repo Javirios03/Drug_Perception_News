93a12032e15834b1a99091929dc0633a0d7d39a7
Francisco Javier Ríos Montes <javier.riosmontes@gmail.com>
2025-08-14 20:31:51 +0200

    Set everything up for Actions' automatization

---
+++ src/automatize.py
@@ -0,0 +1,31 @@
+import datetime
+from yt_dlp import YoutubeDL
+
+today = datetime.date.today()
+
+# Obtain the day in the format dd-mm-yy
+day = today.strftime("%d/%m/%y")
+yesterday = (today - datetime.timedelta(days=2)).strftime("%d/%m/%y")
+
+# Using yt-dlp, extract today's programme URL
+with YoutubeDL({"extract_flat": True, "dump_single_json": True}) as ydl:
+    info = ydl.extract_info("https://www.rtve.es/play/videos/telediario-2/", download=False)
+    
+entries = info.get("entries", [])
+
+# Filter entries for today's date
+url = None
+for entry in entries:
+    # if "title" in entry and day in entry["title"]:
+    if "title" in entry and yesterday in entry["title"]:
+        url = entry.get("url")
+        break
+
+# If no URL found, raise an error
+if not url:
+    # raise ValueError(f"No URL found for the date {day}.")
+    raise ValueError(f"No URL found for the date {yesterday}.")
+
+# Store the URL in a text file
+with open("video_urls.txt", "a") as file:
+    file.write(f"{yesterday} - {url}\n")
