25142935eafd9874a92c9ae736bc97ddc655b81d
Francisco Javier Ríos Montes <javier.riosmontes@gmail.com>
2025-08-14 23:52:53 +0200

    Finish updates for automatization

--- src/automatize.py
+++ src/automatize.py
@@ -5,7 +5,7 @@ today = datetime.date.today()
 
 # Obtain the day in the format dd-mm-yy
 day = today.strftime("%d/%m/%y")
-yesterday = (today - datetime.timedelta(days=2)).strftime("%d/%m/%y")
+# yesterday = (today - datetime.timedelta(days=1)).strftime("%d/%m/%y")
 
 # Using yt-dlp, extract today's programme URL
 with YoutubeDL({"extract_flat": True, "dump_single_json": True}) as ydl:
@@ -17,15 +17,15 @@ entries = info.get("entries", [])
 url = None
 for entry in entries:
     # if "title" in entry and day in entry["title"]:
-    if "title" in entry and yesterday in entry["title"]:
+    if "title" in entry and day in entry["title"]:
         url = entry.get("url")
         break
 
 # If no URL found, raise an error
 if not url:
     # raise ValueError(f"No URL found for the date {day}.")
-    raise ValueError(f"No URL found for the date {yesterday}.")
+    raise ValueError(f"No URL found for the date {day}.")
 
 # Store the URL in a text file
 with open("video_urls.txt", "a") as file:
-    file.write(f"{yesterday} - {url}\n")
+    file.write(f"{day} - {url}\n")
